33d8049c7fcc032ca224d365f9efd87d9bfd436f
cyp0633 <cyp0633@cyp0633.icu>
2022-06-22 20:38:39 +0800

    Rewrite CLI parser

--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/cyp0633/go-drcom
 go 1.18
 
 require (
+	github.com/alecthomas/kingpin v2.2.6+incompatible
 	github.com/go-ini/ini v1.66.6
 	github.com/sevlyar/go-daemon v0.1.5
 )
 
 require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
 	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
 	github.com/stretchr/testify v1.7.4 // indirect
 	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
--- util/parse.go
+++ util/parse.go
@@ -1,8 +1,8 @@
 package util
 
 import (
-	"flag"
 	"fmt"
+	"github.com/alecthomas/kingpin"
 	"os"
 )
 
@@ -18,24 +18,25 @@ var Opt struct {
 	Help    bool
 }
 
+//Parse parse CLI arguments
 func Parse() {
-	flag.StringVar(&Opt.Mode, "m", "", "set your drcom Mode")
+	//kingpin.Arg("mode", "set your drcom mode").Required().StringVar(&Opt.Mode)
+	//kingpin.Arg("conf", "import configuration file").Required().StringVar(&Opt.Conf)
+	//kingpin.Arg("bindip", "bind your ip address").Default("0.0.0.0").StringVar(&Opt.BindIp)
+	//kingpin.Arg("log", "specify log file").StringVar(&Opt.LogPath)
+	kingpin.Flag("mode", "set your drcom mode").Required().Short('m').StringVar(&Opt.Mode)
+	kingpin.Flag("conf", "import configuration file").Required().Short('c').StringVar(&Opt.Conf)
+	kingpin.Flag("bindip", "bind your ip address").Default("0.0.0.0").Short('b').StringVar(&Opt.BindIp)
+	kingpin.Flag("log", "specify log file").Short('l').StringVar(&Opt.LogPath)
+	kingpin.Flag("802.1x", "enable 802.11x").Short('x').BoolVar(&Opt.EnableX)
+	kingpin.Flag("daemon", "set daemon flag").Short('d').BoolVar(&Opt.Daemon)
+	kingpin.Flag("eternal", "set eternal flag").Short('e').BoolVar(&Opt.Eternal)
+	kingpin.Flag("verbose", "set verbose flag").Short('v').BoolVar(&Opt.Verbose)
+	kingpin.HelpFlag.Short('h')
+	kingpin.Parse()
 	if Opt.Mode != "dhcp" && Opt.Mode != "pppoe" {
 		fmt.Println("Unknown mode")
 		os.Exit(1)
 	}
-	flag.StringVar(&Opt.Conf, "c", "", "import configuration file")
 	parseConf(Opt.Conf)
-	flag.StringVar(&Opt.BindIp, "b", "0.0.0.0", "bind your ip address")
-	flag.StringVar(&Opt.LogPath, "l", "", "specify log file")
-	flag.BoolVar(&Opt.EnableX, "x", false, "enable 802.11x")
-	flag.BoolVar(&Opt.Daemon, "d", false, "set daemon flag (Unix-like only)")
-	flag.BoolVar(&Opt.Eternal, "e", false, "set eternal flag")
-	flag.BoolVar(&Opt.Verbose, "v", false, "set verbose flag")
-	flag.BoolVar(&Opt.Help, "h", false, "show help")
-	flag.Parse()
-	if Opt.Help {
-		flag.Usage()
-		os.Exit(0)
-	}
 }
