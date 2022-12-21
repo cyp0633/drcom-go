f4acc20938cf51ef6fd2c46eeb43c40e70d91037
cyp0633 <cyp0633@gmail.com>
2022-12-21 15:16:31 +0800

    Send challenge

--- internal/dhcp/login.go
+++ internal/dhcp/login.go
@@ -1,5 +1,12 @@
 package dhcp
 
+import (
+	"math/rand"
+
+	"github.com/cyp0633/drcom-go/internal/util"
+	"go.uber.org/zap"
+)
+
 // 登录
 func login() (tail []byte, salt []byte, err error) {
 	salt, err = challenge()
@@ -8,5 +15,19 @@ func login() (tail []byte, salt []byte, err error) {
 
 // 发送 challenge 并获得 salt
 func challenge() (salt []byte, err error) {
+	pkt := make([]byte, 20)
+	pkt[0] = 0x01
+	pkt[1] = 0x02
+	pkt[2] = byte(rand.Intn(0xff))
+	pkt[3] = byte(rand.Intn(0xff))
+	pkt[4] = util.Conf.AuthVersion[0]
+	_, err = conn.Write(pkt)
+	if err != nil {
+		util.Logger.Error("Sending challenge failed", zap.Error(err))
+		return nil, err
+	}
+	conn.Flush()
+	util.Logger.Info("Challenge sent", zap.Uint8s("content", pkt))
+	err = ErrorLogin
 	return
 }
--- internal/dhcp/main.go
+++ internal/dhcp/main.go
@@ -4,35 +4,40 @@ package dhcp
 import (
 	"bufio"
 	"net"
+	"time"
 
 	"github.com/cyp0633/drcom-go/internal/util"
 	"go.uber.org/zap"
 )
 
-var writer *bufio.Writer
+var conn *bufio.ReadWriter
 
 // 使用 DHCP 模式上网
 func Run() {
 	util.Logger.Info("DHCP mode")
-	c, err := net.Dial("udp", util.CLI.BindIP+":61440") // 绑定 udp/61440 端口与服务器通信
+	laddr := net.UDPAddr{IP: net.ParseIP(util.CLI.BindIP), Port: 61440}
+	raddr := net.UDPAddr{IP: net.ParseIP(util.Conf.Server), Port: 61440}
+	c, err := net.DialUDP("udp", &laddr, &raddr)
 	if err != nil {
 		util.Logger.Fatal("Open socket on 61440 failed", zap.Error(err))
 	}
+	util.Logger.Info("Opened socket on 61440", zap.String("local", c.LocalAddr().String()), zap.String("remote", c.RemoteAddr().String()))
 	// TODO: 超时动作，需要每次读写设置 deadline？
-	writer = bufio.NewWriter(c)
+	conn = bufio.NewReadWriter(bufio.NewReader(c), bufio.NewWriter(c))
 	defer c.Close()
 	for {
 		tail, salt, err := login()
 		if err != nil {
 			switch err {
 			case ErrorLogin:
+				time.Sleep(5 * time.Second)
 				continue
 			default:
 				util.Logger.Fatal("Login failed", zap.Error(err))
 			}
 		}
 		// 清除 socket buffer
-		err = writer.Flush()
+		err = conn.Flush()
 		if err != nil {
 			util.Logger.Error("Flush socket failed", zap.Error(err))
 		}
