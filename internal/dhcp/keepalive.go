bf81f2b8c3fa63f0e870e40c6f21ba3d49c354cd
cyp0633 <cyp0633@gmail.com>
2022-12-23 15:18:07 +0800

    keepalive2 logic

--- internal/dhcp/keepalive.go
+++ internal/dhcp/keepalive.go
@@ -46,8 +46,9 @@ func keepAlive1(salt []byte, authInfo []byte) error {
 
 var keepAlive2Counter = 0
 
+// 第二个保活包
 func keepAlive2(first *int, encryptType int) error {
-	// send file packet
+	// file packet
 	if *first != 0 {
 		pkt, err := genKeepalive2Packet(first, 1, 0)
 		if err != nil {
@@ -83,9 +84,70 @@ func keepAlive2(first *int, encryptType int) error {
 			return ErrorKeepalive2
 		}
 	}
+	
+	// 心跳包 1 (1/2)
+	*first = 0
+	pkt, err := genKeepalive2Packet(first, 1, 0)
+	if err != nil {
+		return ErrorKeepalive2
+	}
+	keepAlive2Counter++
+	_, err = conn.Write(pkt)
+	if err == nil {
+		err = conn.Flush()
+	}
+	if err != nil {
+		util.Logger.Error("Sending keepalive2 packet failed", zap.Error(err))
+		return ErrorKeepalive2
+	}
+	util.Logger.Debug("Keepalive2_1 sent", zap.String("packet", hex.EncodeToString(pkt)))
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		util.Logger.Error("Receiving keepalive2_1 result failed", zap.Error(err))
+		return ErrorKeepalive2
+	}
+	util.Logger.Debug("Keepalive2_1 recv", zap.String("packet", hex.EncodeToString(buf[:n])))
+	if buf[0] != 0x07 || buf[2] != 0x28 {
+		util.Logger.Warn("Bad keepalive2_1 packet received", zap.String("packet", hex.EncodeToString(buf[:n])))
+		return ErrorKeepalive2
+	}
+	tail := buf[16:20]
+
+	// 心跳包 2 (3/4)
+	pkt, err = genKeepalive2Packet(first, 3, 0)
+	if err != nil {
+		return ErrorKeepalive2
+	}
+	for i := 0; i < 4; i++ {
+		pkt[16+i] = tail[i]
+	}
+	_, err = conn.Write(pkt)
+	if err == nil {
+		err = conn.Flush()
+	}
+	if err != nil {
+		util.Logger.Error("Sending keepalive2_3 packet failed", zap.Error(err))
+		return ErrorKeepalive2
+	}
+	util.Logger.Debug("Keepalive2_3 sent", zap.String("packet", hex.EncodeToString(pkt)))
+
+	buf = make([]byte, 1024)
+	n, err = conn.Read(buf)
+	if err != nil {
+		util.Logger.Error("Receiving keepalive2_3 result failed", zap.Error(err))
+		return ErrorKeepalive2
+	}
+	util.Logger.Debug("Keepalive2_3 recv", zap.String("packet", hex.EncodeToString(buf[:n])))
+	if buf[0] != 0x07 || buf[2] != 0x28 {
+		util.Logger.Warn("Bad keepalive2_3 packet received", zap.String("packet", hex.EncodeToString(buf[:n])))
+		return ErrorKeepalive2
+	}
 	return nil
 }
 
-func genKeepalive2Packet(filepacket *int, typ, encryptType int) (pkt []byte, err error) {
+// 生成第二种保活包
+func genKeepalive2Packet(filepacket *int, typ, encryptType int) (pkt []byte, err error) { // 注意 counter 要 &0xff
 	return
 }
