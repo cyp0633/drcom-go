9010111e13dfa29e6fea8b6a327300a2f9f1eef7
cyp0633 <cyp0633@gmail.com>
2023-03-10 17:16:19 +0800

    Add new dhcp-auto cli entry

--- internal/util/cli.go
+++ internal/util/cli.go
@@ -1,12 +1,13 @@
 package util
 
 var CLI struct {
-	Dhcp    struct{} `cmd:"" help:"Use DHCP mode"`                                                    // DHCP模式
-	Pppoe   struct{} `cmd:"" help:"Use PPPoE mode"`                                                   // PPPoE模式
-	Conf    string   `help:"Configuration file path" short:"c" default:"/etc/drcom.conf" type:"path"` // 配置文件目录
-	BindIP  string   `help:"IP address to bind to" short:"b" default:"0.0.0.0"`                       // 绑定IP地址
-	Log     string   `help:"Log ONLY to specified path" short:"l" default:""`                         // 日志文件目录
-	Daemon  bool     `help:"Run as daemon" short:"d" default:"false"`                                 // 是否在后台运行
-	Eternal bool     `help:"Keep trying to reconnect" short:"e" default:"false"`                      // 是否一直尝试重连
-	Debug   bool     `help:"Print debug level logging" short:"D" default:"false"`                     // 是否打印调试信息
+	Dhcp     struct{} `cmd:"" help:"Use DHCP mode"`                                                    // DHCP模式
+	DhcpAuto struct{} `cmd:"" help:"Use DHCP mode with auto configuration"`                            // DHCP自动配置模式
+	Pppoe    struct{} `cmd:"" help:"Use PPPoE mode"`                                                   // PPPoE模式
+	Conf     string   `help:"Configuration file path" short:"c" default:"/etc/drcom.conf" type:"path"` // 配置文件目录
+	BindIP   string   `help:"IP address to bind to" short:"b" default:"0.0.0.0"`                       // 绑定IP地址
+	Log      string   `help:"Log ONLY to specified path" short:"l" default:""`                         // 日志文件目录
+	Daemon   bool     `help:"Run as daemon" short:"d" default:"false"`                                 // 是否在后台运行
+	Eternal  bool     `help:"Keep trying to reconnect" short:"e" default:"false"`                      // 是否一直尝试重连
+	Debug    bool     `help:"Print debug level logging" short:"D" default:"false"`                     // 是否打印调试信息
 }
--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 
 	"github.com/alecthomas/kong"
 	"github.com/cyp0633/drcom-go/internal/dhcp"
+	dhcpauto "github.com/cyp0633/drcom-go/internal/dhcp/auto"
 	"github.com/cyp0633/drcom-go/internal/util"
+	"go.uber.org/zap"
 )
 
 func main() {
@@ -15,14 +16,17 @@ func main() {
 		kong.Description("Drcom client in Go, see https://github.com/cyp0633/drcom-go"),
 		kong.UsageOnError(),
 	)
-	if util.CLI.Daemon {
-		fmt.Printf("daemon mode\n")
-		daemonize()
-	}
 	util.SetupLog()
-	util.ParseConf()
 	switch ctx.Command() {
+	case "dhcp-auto":
+		dhcpauto.Auto()
+		fallthrough
 	case "dhcp":
+		util.ParseConf()
+		if util.CLI.Daemon {
+			util.Logger.Info("Daemon mode")
+			daemonize()
+		}
 		dhcp.Run()
 	case "pppoe":
 		util.Logger.Fatal("PPPoE mode not implemented")
@@ -33,13 +37,17 @@ func main() {
 func daemonize() {
 	args := os.Args[1:]
 	for i := 0; i < len(args); i++ {
+		// 带 auto 的命令均用于生成配置文件，生成完毕后直接进行普通 DHCP 即可
+		if args[i] == "dhcp-auto" {
+			args[i] = "dhcp"
+		}
+		// 不要重复 fork 了
 		if args[i] == "-d" || args[i] == "--daemon" {
 			args = append(args[:i], args[i+1:]...)
-			break
 		}
 	}
 	cmd := exec.Command(os.Args[0], args...)
 	cmd.Start()
-	fmt.Println("Daemon started with pid", cmd.Process.Pid)
+	util.Logger.Info("Daemon started", zap.Int("pid", cmd.Process.Pid))
 	os.Exit(0)
 }
