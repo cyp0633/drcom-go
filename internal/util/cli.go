2d4e03b127eaa20fb1e951916ae15613ac68cd64
cyp0633 <cyp0633@gmail.com>
2023-04-04 20:04:49 +0800

    Move Daemonize() to util package

--- internal/util/cli.go
+++ internal/util/cli.go
@@ -1,5 +1,12 @@
 package util
 
+import (
+	"os"
+	"os/exec"
+
+	"go.uber.org/zap"
+)
+
 var CLI struct {
 	Dhcp     struct{} `cmd:"" help:"Use DHCP mode"`                                                    // DHCP模式
 	DhcpAuto struct{} `cmd:"" help:"Use DHCP mode with auto configuration"`                            // DHCP自动配置模式
@@ -10,4 +17,23 @@ var CLI struct {
 	Daemon   bool     `help:"Run as daemon" short:"d" default:"false"`                                 // 是否在后台运行
 	Eternal  bool     `help:"Keep trying to reconnect" short:"e" default:"false"`                      // 是否一直尝试重连
 	Debug    bool     `help:"Print debug level logging" short:"D" default:"false"`                     // 是否打印调试信息
+}
+
+// 使程序在后台运行
+func Daemonize() {
+	args := os.Args[1:]
+	for i := 0; i < len(args); i++ {
+		// 带 auto 的命令均用于生成配置文件，生成完毕后直接进行普通 DHCP 即可
+		if args[i] == "dhcp-auto" {
+			args[i] = "dhcp"
+		}
+		// 不要重复 fork 了
+		if args[i] == "-d" || args[i] == "--daemon" {
+			args = append(args[:i], args[i+1:]...)
+		}
+	}
+	cmd := exec.Command(os.Args[0], args...)
+	cmd.Start()
+	Logger.Info("Daemon started", zap.Int("pid", cmd.Process.Pid))
+	os.Exit(0)
 }
--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"os"
-	"os/exec"
-
 	"github.com/alecthomas/kong"
 	"github.com/cyp0633/drcom-go/internal/dhcp"
 	dhcpauto "github.com/cyp0633/drcom-go/internal/dhcp/auto"
 	"github.com/cyp0633/drcom-go/internal/util"
-	"go.uber.org/zap"
 )
 
 func main() {
@@ -25,29 +21,10 @@ func main() {
 		util.ParseConf()
 		if util.CLI.Daemon {
 			util.Logger.Info("Daemon mode")
-			daemonize()
+			util.Daemonize()
 		}
 		dhcp.Run()
 	case "pppoe":
 		util.Logger.Fatal("PPPoE mode not implemented")
 	}
-}
-
-// 使程序在后台运行
-func daemonize() {
-	args := os.Args[1:]
-	for i := 0; i < len(args); i++ {
-		// 带 auto 的命令均用于生成配置文件，生成完毕后直接进行普通 DHCP 即可
-		if args[i] == "dhcp-auto" {
-			args[i] = "dhcp"
-		}
-		// 不要重复 fork 了
-		if args[i] == "-d" || args[i] == "--daemon" {
-			args = append(args[:i], args[i+1:]...)
-		}
-	}
-	cmd := exec.Command(os.Args[0], args...)
-	cmd.Start()
-	util.Logger.Info("Daemon started", zap.Int("pid", cmd.Process.Pid))
-	os.Exit(0)
 }
