18a247fdd4ac9c8364d4b77fe12205600fdf0bfd
cyp0633 <cyp0633@gmail.com>
2024-03-02 00:07:14 +0800

    Fix md5 sum algorithm

--- internal/web/login.go
+++ internal/web/login.go
@@ -3,6 +3,7 @@ package web
 
 import (
 	"crypto/md5"
+	"encoding/hex"
 	"net/http"
 	"net/url"
 
@@ -14,20 +15,18 @@ import (
 func doLogin(server_base string) {
 	username := util.Conf.Username
 	password := util.Conf.Password
-	md5Ctx := md5.New()
-	md5Ctx.Write([]byte("2" + password + "12345678"))
-	cipherStr := md5Ctx.Sum(nil)
-
+	hash := md5.Sum([]byte("2" + password + "12345678"))
+	cipherStr := hex.EncodeToString(hash[:]) + "123456782"
 	form := url.Values{
 		"DDDDD":  {username},
-		"upass":  {string(cipherStr)},
+		"upass":  {cipherStr},
 		"0MKKey": {"123456"},
 		"R1":     {"0"},
 		"R2":     {"1"},
 		"para":   {"00"},
 		"v6ip":   {""},
 	}
-	util.Logger.Debug("Sending login request", zap.String("server", server_base),zap.String("form",form.Encode()))
+	util.Logger.Debug("Sending login request", zap.String("server", server_base), zap.String("form", form.Encode()))
 
 	response, err := http.PostForm(server_base+"/0.htm", form)
 	if err != nil {
